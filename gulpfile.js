0712cdc807bff1c8706dec417d10100fce3c8f14
Frank Kotsianas <fkotsianas@pivotal.io>
2015-12-11 14:54:57 -0800

    Display error output if middleman build task fails.
    [#104929474]

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 var gulp = require('gulp');
 
+
+function displayErrors(err, stdout, stderr) {
+  if(err != undefined) {
+    console.log("\nERROR FOUND\n\n" + err);
+    console.log("\nDUMPING STDOUT\n\n" + stdout);
+    console.log("\nDUMPING STDERR\n\n" + stderr);
+    process.exit();
+  }
+}
+
 gulp.task('middleman', function(cb) {
-  require('child_process').exec('bundle exec middleman build', function() {
+  require('child_process').exec('bundle exec middleman build', function(err, stdout, stderr) {
+    if(err) return displayErrors(err, stdout, stderr);
     cb();
   });
 });
