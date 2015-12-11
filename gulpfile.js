524e7fe38c2d88d4aaf5ee755fa4de29457c5cd0
Frank Kotsianas <fkotsianas@pivotal.io>
2015-12-11 14:56:30 -0800

    Refactor some requires.
    [#104929474]

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
-
+var exec = require('child_process').exec;
+var webserver = require('gulp-webserver');
 
 function displayErrors(err, stdout, stderr) {
   if(err != undefined) {
@@ -11,14 +12,13 @@ function displayErrors(err, stdout, stderr) {
 }
 
 gulp.task('middleman', function(cb) {
-  require('child_process').exec('bundle exec middleman build', function(err, stdout, stderr) {
+  exec('bundle exec middleman build', function(err, stdout, stderr) {
     if(err) return displayErrors(err, stdout, stderr);
     cb();
   });
 });
 
 gulp.task('webserver', function() {
-  var webserver = require('gulp-webserver');
   gulp.src('build').pipe(webserver({
     livereload: true
   }));
